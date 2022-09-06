f461661c3c7112bd65b774211f72d02d0e762803
Alexis Moncada <alecabj12@gmail.com>
2022-09-06 10:19:20 -0500

    init app and login layout and journal layout navbar and sidebar

---
+++ src/journal/Pages/JournalPage.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eos vel numquam dolore eveniet! Aut sint accusantium iste maxime debitis adipisci nisi, assumenda temporibus, quidem, praesentium ducimus magnam et modi.</Typography>
+    </JournalLayout>
+    
+  )
+}
