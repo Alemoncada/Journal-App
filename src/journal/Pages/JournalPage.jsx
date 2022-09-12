b62bf15135574eee3385cc9f71bdd7c72e7d3c5d
Alexis Moncada <alecabj12@gmail.com>
2022-09-12 17:02:56 -0500

    add journalPage with title text and images

--- src/journal/Pages/JournalPage.jsx
+++ src/journal/Pages/JournalPage.jsx
@@ -1,10 +1,12 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eos vel numquam dolore eveniet! Aut sint accusantium iste maxime debitis adipisci nisi, assumenda temporibus, quidem, praesentium ducimus magnam et modi.</Typography>
+      {/* <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eos vel numquam dolore eveniet! Aut sint accusantium iste maxime debitis adipisci nisi, assumenda temporibus, quidem, praesentium ducimus magnam et modi.</Typography> */}
+      {/* < NothingSelectedView/> */}
+      <NoteView />
     </JournalLayout>
     
   )
