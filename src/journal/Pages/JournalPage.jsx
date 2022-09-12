976fd0dd972d58c990aae17e0b3da6d31d0a8571
Alexis Moncada <alecabj12@gmail.com>
2022-09-12 17:26:03 -0500

    basic desing of aplication add button add note

--- src/journal/Pages/JournalPage.jsx
+++ src/journal/Pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 
@@ -5,8 +7,25 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eos vel numquam dolore eveniet! Aut sint accusantium iste maxime debitis adipisci nisi, assumenda temporibus, quidem, praesentium ducimus magnam et modi.</Typography> */}
+      
       {/* < NothingSelectedView/> */}
+
       <NoteView />
+
+      <IconButton
+        size="large"            
+        sx={{
+          color:'white',
+          backgroundColor: 'error.main',
+          ':hover': {backgroundColor: 'error.main', opacity: 0.7},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize:30}}/>
+      </IconButton>
+
     </JournalLayout>
     
   )
